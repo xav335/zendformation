255c8ee5a49264fb5863b27e546905266815a6e7
Xavier Gonzalez <xavier@gonzalez.pm>
2015-01-26 15:11:15 +0100

    creatition module POTM

---
+++ module/POTM/src/POTM/Controller/IndexController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace POTM\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function listAction()
+    {
+        return new ViewModel();
+    }
+
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+    public function updateAction()
+    {
+        return new ViewModel();
+    }
+
+    public function showAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
